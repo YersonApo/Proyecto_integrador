bfd3e88b4bb8f2592230e08f5ca7cf2d7ea66ffd
YersonApo <yaclol567@gmail.com>
2026-05-25 22:49:52 -0500

    Update Planta.java

--- src/main/java/com/miapi/proyectoIntegrador/modelo/Planta.java
+++ src/main/java/com/miapi/proyectoIntegrador/modelo/Planta.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 public class Planta {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nombre;
     private String descripcion;
